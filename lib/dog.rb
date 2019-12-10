d9d87a0e00cdb98d56dffc59b3a94d89a83f7d33
sarah <s.j.condon01@gmail.com>
2019-12-10 16:29:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,7 +14,7 @@ class Dog
     end
     
     def self.new_from_db(row)
-      self.new( id: row[0], name: row[1], breed: row[2])
+      self.new(id: row[0], name: row[1], breed: row[2])
     end
     
     def self.find_by_name(name)
